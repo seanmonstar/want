6e65401c71fa55c75b6fb52f595a692ef3643939
Sean McArthur <sean@seanmonstar.com>
2023-06-14 17:50:47 -0400

    v0.3.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/want/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/want/0.3.1")]
 #![deny(warnings)]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
